a8ea58cc5fb9457467deb1ec6d9ff8e847c9e608
Shabeeh Naqvi <s.naqvi2129@gmail.com>
2025-03-20 03:55:04 +0500

    fix issue with docker and migration

--- system_a/models.py
+++ system_a/models.py
@@ -1,12 +1,12 @@
-import uuid
 from django.db import models
+import uuid
 
 class UserProfile(models.Model):
-    user_id = models.UUIDField(primary_key=True)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15, blank=True)
     address = models.TextField(blank=True)
-    
+
     class Meta:
         app_label = 'system_a'
